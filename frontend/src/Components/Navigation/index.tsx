61d2bcf463ef911d81e1778a9b4e985e5c148030
io-ytsejam <marcin19981@gmail.com>
2020-10-30 19:21:08 +0100

    Push workflow forward

---
+++ frontend/src/Components/Navigation/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {BrowserRouter as Router, Switch} from 'react-router-dom'
+
+export default function Navigation() {
+  return (
+    <Router>
+      <Switch>
+
+      </Switch>
+    </Router>
+  )
+}
