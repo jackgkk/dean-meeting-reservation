23c6e4e2b1cbfde7dfed28d3ba235fc0d4c76a29
mialek <michalaleksanderk@gmail.com>
2020-11-15 17:42:10 +0100

    updated controller

--- src/main/java/com/iww/deanmeetingreservations/Controllers/CalendarController.java
+++ src/main/java/com/iww/deanmeetingreservations/Controllers/CalendarController.java
@@ -1,17 +1,18 @@
 package com.iww.deanmeetingreservations.Controllers;
 
+import org.apache.coyote.Response;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.*;
 
 @Controller
 public class CalendarController {
@@ -118,7 +119,9 @@ public class CalendarController {
         }
     }
 
-    @RequestMapping(value = "/api/dean/calendar/get-confirmed-meetings", method = RequestMethod.GET)
+    //page implementation for get-confirmed-meetings
+
+    /*@RequestMapping(value = "/api/dean/calendar/get-confirmed-meetings", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<Page<MockReturn>> getMockup(Pageable pageable) {
 
@@ -129,6 +132,24 @@ public class CalendarController {
                         new MockGuest("Beta","Orbiter","BO@email.com","Mock_Status2"))),pageable,2);
 
         return new ResponseEntity(page,HttpStatus.OK);
+    }*/
+
+    @RequestMapping(value = "/api/dean/calendar/get-confirmed-meetings", method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<List<MockReturn>> getMockup() {
+
+        List<MockReturn> list = new ArrayList<>(List.of(new MockReturn("UUID1","MockDescription1","01.01.2021",15.5, true,
+                        new MockGuest("Jan","Paweł","JP@acab.pl","Mock_Status1")),
+                new MockReturn("UUID2","MockDescription2","02.01.2021",10.0, false,
+                        new MockGuest("Beta","Orbiter","BO@email.com","Mock_Status2"))));
+        return new ResponseEntity(list,HttpStatus.OK);
     }
 
+    @RequestMapping(value = "/api/dean/calendar/cancel-meeting/{id}",
+            method = RequestMethod.DELETE)
+    @ResponseStatus(code = HttpStatus.OK)
+    public void mockDelete(@PathVariable("id") final UUID id) {
+        if(id.hashCode()%2==0)
+            throw new ResponseStatusException(HttpStatus.I_AM_A_TEAPOT);
+    }
 }
