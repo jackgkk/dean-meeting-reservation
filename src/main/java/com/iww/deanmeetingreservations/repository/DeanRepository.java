91e01c5f97d39f9283fc263291bb10c8dab4284d
czarus98 <czarek.wojcik98@gmail.com>
2020-11-27 19:08:53 +0100

    Changed type of id columns

--- src/main/java/com/iww/deanmeetingreservations/repository/DeanRepository.java
+++ src/main/java/com/iww/deanmeetingreservations/repository/DeanRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DeanRepository extends JpaRepository<Dean, Long> {
+public interface DeanRepository extends JpaRepository<Dean, String> {
 }
