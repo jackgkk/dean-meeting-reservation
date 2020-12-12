469d9b2d38850be14330702aa2f61ece6f4d8f5c
czarus98 <czarek.wojcik98@gmail.com>
2020-12-12 19:38:46 +0100

    Dean info endpoints modification to handle jwt token

--- src/main/java/com/iww/deanmeetingreservations/DeanMeetingReservationsApplication.java
+++ src/main/java/com/iww/deanmeetingreservations/DeanMeetingReservationsApplication.java
@@ -1,15 +1,7 @@
 package com.iww.deanmeetingreservations;
 
-import com.iww.deanmeetingreservations.model.Dean;
-import com.iww.deanmeetingreservations.repository.DeanRepository;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DeanMeetingReservationsApplication {
--- src/main/java/com/iww/deanmeetingreservations/service/DeanInfoServiceImpl.java
+++ src/main/java/com/iww/deanmeetingreservations/service/DeanInfoServiceImpl.java
@@ -1,17 +1,19 @@
 package com.iww.deanmeetingreservations.service;
 
+import com.iww.deanmeetingreservations.config.JwtTokenUtil;
 import com.iww.deanmeetingreservations.dto.DeanDto;
 import com.iww.deanmeetingreservations.dto.DeanInfoDto;
 import com.iww.deanmeetingreservations.model.Dean;
 import com.iww.deanmeetingreservations.model.DeanDepartment;
 import com.iww.deanmeetingreservations.model.Department;
+import com.iww.deanmeetingreservations.model.Duty;
 import com.iww.deanmeetingreservations.repository.DeanDepartmentRepository;
 import com.iww.deanmeetingreservations.repository.DeanRepository;
 import com.iww.deanmeetingreservations.repository.DepartmentRepository;
-import org.modelmapper.ModelMapper;
+import com.iww.deanmeetingreservations.repository.DutyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -20,66 +22,91 @@ import java.util.Optional;
 public class DeanInfoServiceImpl implements DeanInfoService {
 
     @Autowired
-    DeanRepository deanRepository;
+    private DeanRepository deanRepository;
 
     @Autowired
-    DepartmentRepository departmentRepository;
+    private DepartmentRepository departmentRepository;
 
     @Autowired
-    DeanDepartmentRepository deanDepartmentRepository;
+    private DeanDepartmentRepository deanDepartmentRepository;
 
     @Autowired
-    ModelMapper modelMapper;
+    private DeanService deanService;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private DutyRepository dutyRepository;
 
     @Override
-    public void updateProfile(DeanDto deanDto, String id) {
+    public void updateProfile(DeanDto deanDto, String id, String token) {
         Optional<Dean> dean = deanRepository.findById(id);
+        token = token.substring(7);
         if (dean.isPresent()) {
-            Dean deanModel = dean.get();
-            if (deanDto.getFirstname() != null) {
-                deanModel.setFirstname(deanDto.getFirstname());
-            }
-            if (deanDto.getLastname() != null) {
-                deanModel.setLastname(deanDto.getLastname());
-            }
-            if (deanDto.getEmail() != null) {
-                deanModel.setEmail(deanDto.getEmail());
-            }
-            if (deanDto.getPassword() != null) {
-                deanModel.setPassword(deanDto.getPassword());
-            }
-            if (deanDto.getDepartment() != null) {
-                Department department = departmentRepository.getFirstByDepartmentNameEquals(deanDto.getDepartment()).orElse(null);
-                if (department == null) {
-                    department = new Department(deanDto.getDepartment());
-                    departmentRepository.save(department);
-                    departmentRepository.flush();
-                    department = departmentRepository.getFirstByDepartmentNameEquals(deanDto.getDepartment()).get();
+            if(jwtTokenUtil.validateToken(token, deanService.loadUserByUsername(dean.get().getEmail()))){
+                Dean deanModel = dean.get();
+                if (deanDto.getFirstname() != null) {
+                    deanModel.setFirstname(deanDto.getFirstname());
                 }
-                DeanDepartment deanDepartment = new DeanDepartment(deanModel, department);
-                deanModel.addDeanDepartment(deanDepartment);
-                department.addDeanDepartment(deanDepartment);
-                deanDepartmentRepository.saveAndFlush(deanDepartment);
-                departmentRepository.saveAndFlush(department);
+                if (deanDto.getLastname() != null) {
+                    deanModel.setLastname(deanDto.getLastname());
+                }
+                if (deanDto.getEmail() != null) {
+                    deanModel.setEmail(deanDto.getEmail());
+                }
+                if (deanDto.getPassword() != null) {
+                    deanModel.setPassword(deanDto.getPassword());
+                }
+                if (deanDto.getDepartment() != null) {
+                    Department department = departmentRepository.getFirstByDepartmentNameEquals(deanDto.getDepartment()).orElse(null);
+                    if (department == null) {
+                        department = new Department(deanDto.getDepartment());
+                        departmentRepository.save(department);
+                        departmentRepository.flush();
+                        department = departmentRepository.getFirstByDepartmentNameEquals(deanDto.getDepartment()).get();
+                    }
+                    DeanDepartment deanDepartment = new DeanDepartment(deanModel, department);
+                    deanModel.addDeanDepartment(deanDepartment);
+                    department.addDeanDepartment(deanDepartment);
+                    deanDepartmentRepository.saveAndFlush(deanDepartment);
+                    departmentRepository.saveAndFlush(department);
+                }
+                if (deanDto.getDuties()!=null) {
+                    Duty newDuty;
+                    for (Duty d : deanDto.getDuties()) {
+                        newDuty = new Duty(d.getDayOfTheWeek(), d.getStartsAt(), d.getEndsAt());
+                        deanModel.addDuty(newDuty);
+                    }
+                }
+                deanRepository.saveAndFlush(deanModel);
             }
-            deanRepository.saveAndFlush(deanModel);
         } else {
             throw new ResourceNotFoundException();
         }
     }
 
     @Override
-    public DeanInfoDto findUserById(String id) {
+    public DeanInfoDto findUserById(String id, String token) {
         Optional<Dean> dean = deanRepository.findById(id);
+        token = token.substring(7);
         if (dean.isPresent()) {
-            return new DeanInfoDto(dean.get().getFirstname(), dean.get().getLastname(), dean.get().getEmail(), dean.get().getDuties());
+            if(jwtTokenUtil.validateToken(token, deanService.loadUserByUsername(dean.get().getEmail()))){
+                return new DeanInfoDto(dean.get().getFirstname(), dean.get().getLastname(), dean.get().getEmail(), dean.get().getDuties());
+            }
+            throw new SecurityException();
         } else {
             throw new ResourceNotFoundException();
         }
     }
 
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
+    @Override
+    public void deleteByDutyId(String dutyId, String token) throws BadCredentialsException,ResourceNotFoundException {
+        token = token.substring(7);
+        if(!dutyRepository.existsById(dutyId))
+            throw new ResourceNotFoundException("No duty with given id");
+        if(!dutyRepository.findById(dutyId).get().getDeanEmail().equals(jwtTokenUtil.getEmailFromToken(token)))
+            throw new BadCredentialsException("Can't delete someone else's duty");
+        dutyRepository.deleteById(dutyId);
     }
 }
